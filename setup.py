b460d0a7af606779997ed93b80aad3b45068e895
Edgar Valdez <edgarvaldez99@gmail.com>
2023-06-13 15:42:05 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paquete-fe',
+    packages=find_packages(),
+    package_dir={'paquete_fe': '.'},
+    version='0.1.23',
+    description='Facturación Electrónica',
+    author='Mateo Nunez',
+    author_email='mateo@idl.com.py',
+    url='https://cds.com.py',
+    include_package_data=True,
+    install_requires=[
+        'lxml',
+        'SQLAlchemy',
+        'pyOpenSSL',
+        'zeep',
+        'requests',
+        'pytz',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
