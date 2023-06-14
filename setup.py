a745b90be5f1fb0ababb14e6569cca04350985ae
Edgar Valdez <edgarvaldez99@gmail.com>
2023-06-13 22:17:11 -0400

    added modules

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='custom-package-py',
+    name='custom_package_py',
     packages=find_packages(),
     package_dir={'custom_package_py': '.'},
     version='0.0.0',
